b5d07f1d2d081f85bbf1d816b8703007e0d25476
Diego Andres Arbelaez <diegoandresarbelaez@gmail.com>
2023-03-24 09:14:42 -0500

    Login background image styles

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,7 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        return View("~/Views/Home/Login/loginForm.cshtml");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
