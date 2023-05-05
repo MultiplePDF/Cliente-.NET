78395ce0b1a63b9b1abd7e20f613ccb1c76ad172
DAArbelaez <123782359+DAArbelaez@users.noreply.github.com>
2023-05-04 19:57:16 -0500

    Feature: Convert files back (#16)
    * Add initial structure
    
    * Upload files
    
    * Fix json
    
    * Change filename json
    
    * Fix image downloads
    
    * Fix code smells
    
    * Add Logout
    
    ---------
    
    Co-authored-by: Daniel Salazar <dansasx@gmail.com>

--- Controllers/MainPageController.cs
+++ Controllers/MainPageController.cs
@@ -1,6 +1,9 @@
 using System.Security.Cryptography;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using ServiceReference1;
+
 
 namespace client.Controllers
 {
@@ -13,6 +16,12 @@ namespace client.Controllers
         {
             _logger = logger;
             this.hostingEnvironment = hostingEnvironment;
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Response.Cookies.Delete("token");
+            return RedirectToAction("Index", "Home");
         }
 
         private IActionResult RedirectToMainIfTokenExists(string viewName)
@@ -36,10 +45,13 @@ namespace client.Controllers
                 var fileName = fileItem.FileName;
                 FileInfo fi = new FileInfo(fileName);
                 var fileNameExt = fi.Extension;
-                string[] subs = fileName.Split('.');
-                var justFileName = subs[0];
+                String justFileName = Path.GetFileNameWithoutExtension(fileName);
 
-                var fileContent = string.Empty;
+                //Unique name
+                string fileNameWithoutSpecialChars = Regex.Replace(justFileName, "[^0-9a-zA-Z._-]", "");
+                string uniqueName = DateTime.Now.Ticks.ToString() + "_" + fileNameWithoutSpecialChars;
+
+                //Size
                 var fileSizeInKb = (int)fileItem.Length / 1000;
                 StreamReader sr = new StreamReader(fileItem.OpenReadStream());
 
@@ -50,13 +62,12 @@ namespace client.Controllers
                 fileItem.OpenReadStream().CopyTo(memoryStream);
                 byte[] byteArray = memoryStream.ToArray();
 
-
                 var base64String = Convert.ToBase64String(byteArray);
                 var fileInfo = new
                 {
                     idFile = idFile,
                     base64 = base64String,
-                    fileName = justFileName,
+                    fileName = uniqueName,
                     fileExtension = fileNameExt,
                     size = fileSizeInKb,
                     checksum = checksum
@@ -65,17 +76,51 @@ namespace client.Controllers
                 results.Add(fileInfo);
             }
             var json = JsonConvert.SerializeObject(results);
+
             return Content(json, "application/json");
+
+            //Send to batch 
+            var token = HttpContext.Request.Cookies["token"];
+            var res = ConvertFiles(json, token);
+            var download = res.downloadPath;
+
+            if (res.successful)
+            {
+                return View("Views/MainPage/Downloads/Downloads.cshtml");
+            }
+            else
+            {
+                ViewData["ErrorMessage"] = res.response;
+                return View("Views/MainPage/Index.cshtml");
+
+            }
+
             //return RedirectToAction("Index");
+        }
+
+        public sendBatchResponse ConvertFiles(String json, String token)
+        {
+            MultiplepdfPortClient client = new();
+            sendBatchRequest req = new();
+            req.listJSON = json;
+            req.token = token;
+            sendBatchResponse res = client.sendBatch(req);
+            return res;
         }
 
         public string GetSha256HashFromIFormFile(IFormFile file)
         {
-            using var stream = file.OpenReadStream();
-            using var sha256 = SHA256.Create();
-            var hashBytes = sha256.ComputeHash(stream);
-            var hashString = BitConverter.ToString(hashBytes).Replace("-", "");
-            return hashString;
+            byte[] hashBytes;
+
+            using (var sha256 = SHA256.Create())
+            {
+                using (var stream = file.OpenReadStream())
+                {
+                    hashBytes = sha256.ComputeHash(stream);
+                }
+            }
+            string hashValue = BitConverter.ToString(hashBytes).Replace("-", string.Empty).ToLower();
+            return hashValue;
         }
 
 
